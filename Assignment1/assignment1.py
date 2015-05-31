9b6177075771c3264fcbe12011ff723714e80b52
Thomas Sonderman <trsonderm@gmail.com>
2015-05-31 10:03:01 -0400

    added independent py

---
+++ Assignment1/assignment1.py
@@ -0,0 +1,29 @@
+# Thomas Sonderman
+# tsonderm@fau.edu
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import rcParams
+#define header row names for reading the csv
+headers = ['package_id', 'DataSet Title', 'Organization Name', 'Views','Date']
+df = pd.read_csv('datagovdatasetsviewmetrics.csv',header=False,
+                 names=headers)
+#Run SQL Commands, Group the views by org name, sum the views, then sort the records by Total Views and grab the top 10
+grouped = df[['Organization Name','Views']].groupby('Organization Name').sum().sort(columns='Views',ascending=False).head(10)
+#Bar Chart Section
+print grouped.to_string()
+#add autolayout so labels aren't cutoff
+rcParams.update({'figure.autolayout': True})
+#initialize matplotlib
+plt.figure()
+#organize data layout for graph
+my_plot = grouped.plot(kind='barh',legend="Views",title="Total Views",figsize=(10, 7))
+my_plot.set_xlabel("Organizations")
+
+#display graph
+plt.show()
+plt.close()
+
+
+
